aaee39744ef15c4687cfc3c2bd97a7e647f90bf7
Vishnu Vardhan Maheswaran <vishnu191299@gmail.com>
2024-03-11 14:52:29 -0400

    Time: 109 ms (69.03%), Space: 19.2 MB (31.07%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                del d[i]
+        return list(d.keys())[0]
