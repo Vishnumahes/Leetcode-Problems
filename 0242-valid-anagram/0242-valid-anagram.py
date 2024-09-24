70ea3edbc68ea455d43e0c610c0ecdc5a1a3710c
Vishnu Vardhan Maheswaran <vishnu191299@gmail.com>
2024-09-23 23:20:25 -0700

    Time: 53 ms (35.00%), Space: 16.9 MB (48.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(s)):
             CountS[s[i]]=1+CountS.get(s[i],0)
             CountT[t[i]]=1+CountT.get(t[i],0)
-        return CountS==CountT
+        return CountS == CountT
             
 
         
