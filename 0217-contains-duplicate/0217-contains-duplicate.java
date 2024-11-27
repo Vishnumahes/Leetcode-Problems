96a53e9c1268e94e33c6a570ddd89cdce722abe9
Vishnu Vardhan Maheswaran <vishnu191299@gmail.com>
2024-11-27 10:43:49 -0800

    Time: 11 ms (71.82%), Space: 57.8 MB (57.97%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer>samenumber= new HashSet<>();
+        for(int num:nums){
+            if(samenumber.contains(num)){
+                return true;
+            }
+            samenumber.add(num);
+        }
+        return false;
+    }
+    
+}
